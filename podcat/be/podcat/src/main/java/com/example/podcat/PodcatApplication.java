0bd5eb8112eb70a754f1cf0caf48142290c56ea0
Dinhhuy0810 <102515280+HuyXinLoi@users.noreply.github.com>
2025-05-18 20:01:32 +0700

    Update PodcatApplication.java

--- podcat/be/podcat/src/main/java/com/example/podcat/PodcatApplication.java
+++ podcat/be/podcat/src/main/java/com/example/podcat/PodcatApplication.java
@@ -10,18 +10,18 @@ public class PodcatApplication {
 
 	public static void main(String[] args) {
 		// Load .env
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./be/podcat")
-                .filename(".env")
-                .load();
+        // Dotenv dotenv = Dotenv.configure()
+        //         .directory("./be/podcat")
+        //         .filename(".env")
+        //         .load();
 
-        // Set ENV
-        System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-        System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+        // // Set ENV
+        // System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+        // System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        // System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+        // System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+        // System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+        // System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
 		SpringApplication.run(PodcatApplication.class, args);
 	}
 
