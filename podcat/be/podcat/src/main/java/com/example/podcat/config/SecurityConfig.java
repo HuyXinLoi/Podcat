59b75cda7594a9f7c6df875ce2aef3824ea62130
HuyXinLoi <huyhodinh0909@gmail.com>
2025-06-21 20:17:40 +0700

    config port for localhost check reponsive ui

--- podcat/be/podcat/src/main/java/com/example/podcat/config/SecurityConfig.java
+++ podcat/be/podcat/src/main/java/com/example/podcat/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://demowebappflutter.vercel.app", "http://localhost:*"));
+        config.setAllowedOrigins(List.of("https://demowebappflutter.vercel.app", "http://localhost:53676"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
